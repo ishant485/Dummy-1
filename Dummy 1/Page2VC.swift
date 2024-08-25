3f7c8f1fbe5e9dc27cc4dc94054a74d285c537c3
Ishant Juneja <ishant4852@gmail.com>
2024-08-25 17:33:01 +0530

    Added page 2

---
+++ Dummy 1/Page2VC.swift
@@ -0,0 +1,27 @@
+//
+//  Page2VC.swift
+//  Dummy 1
+//
+//  Created by Ishant Juneja on 25/08/24.
+//
+
+import UIKit
+
+class Page2VC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Dummy 1/ViewController.swift
+++ Dummy 1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var continueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func continueAction(_ sender: Any) {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let pg2vc = storyBoard.instantiateViewController(withIdentifier: "Page2VC") as? Page2VC
+        self.navigationController?.pushViewController(pg2vc!, animated: true)
+    }
 }
 
